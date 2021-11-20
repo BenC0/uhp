988c9573629ec5fa8fcfe0464895f1f02e81c896
BenC0 <suddensquirrel94@gmail.com>
2021-11-20 23:25:59 +0000

    ran around in circles trying to get a wall to collide with the player

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,21 +3,9 @@ extends Area2D
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 
-func move_player(velocity, delta):
-	$AnimatedSprite.animation = "walk"
-	$AnimatedSprite.flip_h = velocity.x < 0
-	velocity = velocity.normalized() * speed
-	$AnimatedSprite.play()
-	
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
@@ -28,8 +16,27 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-
 	if velocity.length() > 0:
-		move_player(velocity, delta)
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "walk"
+
+func start(pos):
+	position = pos
+	$CollisionPolygon2D.disabled = false
+
+func _on_Player_area_entered(_body):
+	print("somethign")
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionPolygon2D.set_deferred("disabled", true)
