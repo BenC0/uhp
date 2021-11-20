85b2238a2849e22c1e67b0ac9792bebcc19b0228
BenC0 <suddensquirrel94@gmail.com>
2021-11-20 22:38:11 +0000

    Basic movement added

---
+++ Player/Player.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func move_player(velocity, delta):
+	$AnimatedSprite.animation = "walk"
+	$AnimatedSprite.flip_h = velocity.x < 0
+	velocity = velocity.normalized() * speed
+	$AnimatedSprite.play()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		move_player(velocity, delta)
+	else:
+		$AnimatedSprite.stop()
